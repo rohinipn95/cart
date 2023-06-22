6cddc064aeeb3aa41fac173d3813e2c5eb3a4639
RohiniPN <rohinipn95@gmail.com>
2023-06-22 11:54:07 +0530

    cart

---
+++ shopeapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from shopeapp import views
+app_name='shopeapp'
+urlpatterns = [
+    path('',views.allProdCat,name='allProdCat'),
+    path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatDetail')
+    ]
